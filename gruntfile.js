627c2852a40ccc22e4853fd92ab145829c955acc
Oleg Tarasov <041616@gmail.com>
2015-11-30 01:46:35 +0200

    commit #1

--- gruntfile.js
+++ gruntfile.js
@@ -1,49 +1,36 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-
     stylus: {
       compile: {
         options: {
-          compress: true,
-          paths: ['path/to/import', 'another/to/import'],
-          relativeDest: '../out', //path to be joined and resolved with each file dest to get new one.
-                                  //mostly useful for files specified using wildcards
-          urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
-          use: [
-            function () {
-              return testPlugin('yep'); // plugin with options
-            },
-            require('fluidity') // use stylus plugin at compile time
-          ],
-          import: [      //  @import 'foo', 'bar/moo', etc. into every .styl file
-            'foo',       //  that is compiled. These might be findable based on values you gave
-            'bar/moo'    //  to `paths`, or a plugin you added under `use`
-          ]
+          compress: true
         },
-        files: {
-          'path/to/result.css': 'path/to/source.styl', // 1:1 compile
-          'path/to/another.css': ['path/to/sources/*.styl', 'path/to/more/*.styl'] // compile and concat into single file
-        }
+        files: [{
+            expand: true,
+            cwd: "stylus/themes/",
+            src: "*.styl",
+            dest: "css",
+            ext: ".css"
+        }]
       }
+    },
+    watch: {
+      options: {
+        dateFormat: function(time) {
+          grunt.log.writeln("The watch finished in " + time + "ms.");
+          grunt.log.writeln("Waiting...");
+        },
+      },
+      stylus: {
+        files: "stylus/**/*.styl",
+        tasks: "stylus",
+      },
     }
-
-    // uglify: {
-    //   options: {
-    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-    //   },
-    //   build: {
-    //     src: 'src/<%= pkg.name %>.js',
-    //     dest: 'build/<%= pkg.name %>.min.js'
-    //   }
-    // }
   });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-contrib-stylus");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+  grunt.registerTask("default", ["stylus"]);
 };
