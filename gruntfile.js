92b22f51cbf79842dd92d36c00742707d79384d9
otarasov <otarasov@otarasov.(none)>
2015-11-30 19:32:59 +0200

    commit #2

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,7 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    stylus: {
-      compile: {
-        options: {
-          compress: true
-        },
-        files: [{
-            expand: true,
-            cwd: "stylus/themes/",
-            src: "*.styl",
-            dest: "css",
-            ext: ".css"
-        }]
-      }
-    },
+  var configObject = {
+    stylus: {},
     watch: {
       options: {
         dateFormat: function(time) {
@@ -27,7 +14,53 @@ module.exports = function(grunt) {
         tasks: "stylus",
       },
     }
+  };
+
+  grunt.file.recurse("themes/", function(abspath, rootdir, subdir, filename){
+    if (grunt.file.isFile(abspath)){
+      settings = grunt.file.readJSON(abspath);
+      grunt.log.writeln(abspath);
+      grunt.log.writeln(rootdir);
+      grunt.log.writeln(subdir);
+      grunt.log.writeln(filename);
+    }
   });
+
+  //for (var prop in config.themes) {
+  //  configObject.stylus[themeFilename] = {
+  //    options: {
+  //      compress: true,
+  //      define: ""
+  //    },
+  //    files: [{
+  //        expand: true,
+  //        cwd: "stylus/",
+  //        src: "main.styl",
+  //        dest: "css",
+  //        ext: ".css"
+  //    }]
+  //  };
+  //  grunt.log.writeln(prop+": "+config.themes[prop]);
+  //}
+
+
+  grunt.config.init(configObject);
+  //grunt.initConfig({
+  //  stylus: {
+  //    compile: {
+  //      options: {
+  //        compress: true
+  //      },
+  //      files: [{
+  //          expand: true,
+  //          cwd: "stylus/themes/",
+  //          src: "*.styl",
+  //          dest: "css",
+  //          ext: ".css"
+  //      }]
+  //    }
+  //  }
+  //});
 
   grunt.loadNpmTasks("grunt-contrib-stylus");
   grunt.loadNpmTasks("grunt-contrib-watch");
