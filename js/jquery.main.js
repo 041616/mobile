ece6588acb60f4eb6f0b663d3f57be0bd2a6c9ab
041616 <041616@gmail.com>
2016-01-12 19:17:54 +0200

    commit #9

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,30 +1,32 @@
 $(document).ready(function(){
-    $('#swiper').css({
+    $('#content').css({
         top: $('#top-panel').outerHeight(true),
         bottom: $('#bottom-panel').outerHeight(true)
     });
-    var mySwiper = new Swiper ('.swiper', {
-        autoplay: 5000,
-        nextButton: '.swiper__btn_direction_right',
-        prevButton: '.swiper__btn_direction_left',
-        buttonDisabledClass: 'swiper__btn_state_disable',
-        wrapperClass: 'swiper__list',
-        slideClass: 'swiper__item',
-        slideActiveClass: 'swiper__item_state_active',
-        slideNextClass: 'swiper__item_position_next',
-        slidePrevClass: 'swiper__item_position_prev'
-    });
+    if (typeof Swiper === 'function') {
+        var mySwiper = new Swiper('.swiper', {
+            autoplay: 5000,
+            nextButton: '.swiper__btn_direction_right',
+            prevButton: '.swiper__btn_direction_left',
+            buttonDisabledClass: 'swiper__btn_state_disable',
+            wrapperClass: 'swiper__list',
+            slideClass: 'swiper__item',
+            slideActiveClass: 'swiper__item_state_active',
+            slideNextClass: 'swiper__item_position_next',
+            slidePrevClass: 'swiper__item_position_prev'
+        });
+    };
 });
 
 $(window).resize(function() {
-    $('#swiper').css({
+    $('#content').css({
         top: $('#top-panel').outerHeight(true),
         bottom: $('#bottom-panel').outerHeight(true)
     });
 });
 
 $(window).on("orientationchange",function(event){
-    $('#swiper').css({
+    $('#content').css({
         top: $('#top-panel').outerHeight(true),
         bottom: $('#bottom-panel').outerHeight(true)
     });
